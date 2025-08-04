f6131b26a54657b161cde2b8d97faaac883ac059
elsayed-elkhouly <seko300mo@gmail.com>
2025-08-04 19:35:47 +0300

    تعديل ملفات Portfolio و Footer وإضافة فولدر card و modual

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -5,9 +5,9 @@ const About = () => {
     return (
         <>
 
-    <div className="bg-danger py-5  text-white h-100">
+    <div className="about py-4  text-white h-100">
                 <div className="container">
-                <div className="text-center justify-content-between align-items-center ">
+                <div className="text-center justify-content-between align-items-center  my-5">
                     <h1>ABOUT CONPONENT</h1>
                     <div className="d-flex justify-content-center align-items-center ss">
 
@@ -17,7 +17,7 @@ const About = () => {
 
 
                 </div>
-                <div className="row pt-4">
+                <div className="row py-4">
                     <div className="col-md-6">
                         <p>
                             Freelancer is a free bootstrap theme created by Route. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.
